bcaf36973eef60619c5a9d8a4cfbb7473cb04a56
Samuel Kendall <kinguser981@gmail.com>
2023-12-28 01:32:18 +0330

    Update BoardConfig.mk for OrangeFox
    Signed-off-by: Samuel Kendall <kinguser981@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -130,6 +130,7 @@ TW_NO_FLASH_CURRENT_TWRP := true # For A/B devices that has dedicated recovery, 
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_HAS_MTP := true
 TW_FORCE_KEYMASTER_VER := true
+OF_DEFAULT_KEYMASTER_VERSION := 4.1
 TW_Y_OFFSET := 60
 TW_H_OFFSET := -60
 TW_FRAMERATE := 60
