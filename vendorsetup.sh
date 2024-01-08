a9c2c8bf0ce33ebe91687285a4a7ec4fffeac05c
Samuel Kendall <kinguser981@gmail.com>
2024-01-08 10:22:03 +0330

    Update vendorsetup.sh
    Signed-off-by: Samuel Kendall <kinguser981@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -50,7 +50,7 @@ if [ "$1" = "$FDEVICE" -o "$FOX_BUILD_DEVICE" = "$FDEVICE" ]; then
  export OF_ENABLE_LPTOOLS=1
 
  # Version & Variant
- export FOX_VERSION="R12.1"
+ export FOX_VERSION="R11.1"
  export FOX_VARIANT="A14"
  export FOX_BUILD_TYPE="stable"
 
