1610207ad7c4c726d740081473a64f16a833f851
Samuel Kendall <kinguser981@gmail.com>
2024-01-08 05:04:22 +0330

    Update vendorsetup.sh
    Signed-off-by: Samuel Kendall <kinguser981@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,7 +6,7 @@
 #	it under the terms of the GNU General Public License as published by
 #	the Free Software Foundation, either version 3 of the License, or
 #	any later version.
-# SPDX-License-Identifier: Apache-2.0
+# 	 SPDX-License-Identifier: Apache-2.0
 #
 #	OrangeFox is distributed in the hope that it will be useful,
 #	but WITHOUT ANY WARRANTY; without even the implied warranty of
