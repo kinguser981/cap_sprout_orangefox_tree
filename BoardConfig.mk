e30b1996fba2043aba0991eecb10728732d395cf
Samuel Kendall <kinguser981@gmail.com>
2024-08-28 18:51:49 +0330

    Update BoardConfig.mk
    Signed-off-by: Samuel Kendall <kinguser981@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -142,5 +142,12 @@ TW_FRAMERATE := 60
 #ENABLE_SCHEDBOOST := true
 #TARGET_USES_UEFI := true
 
+# mke2fs
+TARGET_USES_MKE2FS := true
+
+TW_OVERRIDE_SYSTEM_PROPS := \
+    "ro.build.product;ro.build.fingerprint=ro.system.build.fingerprint;ro.build.version.incremental;ro.product.device=ro.product.system.device;ro.product.model=ro.product.system.model;ro.product.name=ro.product.system.name"
+
+
 # Maintainer/Version
 include $(DEVICE_PATH)/version.mk
